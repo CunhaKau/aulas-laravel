744ae4f967b4ec5a07feeed791cffd0f6a1791f6
ANA CLAUDIA SOUZA CUNHA <ana.cscunha1@senacsp.edu.br>
2025-06-09 11:42:27 -0300

    aula 1

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/sobrenos', function (){
+    return 'Essa é a página sobre nós';
+});
