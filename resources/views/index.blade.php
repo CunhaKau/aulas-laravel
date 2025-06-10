8051c42bdff1c688790c7c2910b57e4d041275ee
ANA CLAUDIA SOUZA CUNHA <ana.cscunha1@senacsp.edu.br>
2025-06-10 12:00:26 -0300

    aula 10-06

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,16 +13,24 @@
     <meta name="generator" content="Astro v5.7.10">
     <title>Dashboard Template · Bootstrap v5.3</title>
     
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
     <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT">
-    <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
+    
+        <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{ asset("icons/apple-touch-icon.png") }}" sizes="180x180">
+    <link rel="icon" href="{{ asset("icons/favicon-32x32.png") }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ asset("icons/favicon-16x16.png") }}" sizes="16x16" type="image/png">
+    <link rel="mask-icon" href="{{ asset("icons/safari-pinned-tab.svg") }}" color="#712cf9">
+    <link rel="icon" href="{{ asset("icons/favicon.ico") }}">
     <meta name="theme-color" content="#712cf9">
+
+    <!-- Customizando o estilo do template -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
+
+     <!-- CONTINUAR -->
+
     <link href="dashboard.css" rel="stylesheet">
     <style>
         .bd-placeholder-img {
