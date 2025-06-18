1336b6983974ebae9b52b1b1fd094d090cc95b6c
ANA CLAUDIA SOUZA CUNHA <ana.cscunha1@senacsp.edu.br>
2025-06-18 10:57:22 -0300

    create

---
+++ app/Http/Controllers/ContatosController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contatos;
+
+class ContatosController extends Controller
+{
+    public function index(){
+        $findContatos = Contatos::get();
+
+        return view('pages.contatos.index', compact('findContatos'));
+    }
+
+    public function delete($idUser){
+        $buscaRegistro = Contatos::find($idUser);
+        $buscaRegistro->delete();
+
+        return back();
+    }
+
+    public function create(Request $request){
+
+        //condicional para entendimento do envio dos dados para o banco de dados
+        if($request->method() == "POST"){
+            $data  = $request->all();
+            Contatos::create($data);
+
+            return redirect('/contatos');
+        }
+
+        return view('pages.contatos.create');
+    }
+}
